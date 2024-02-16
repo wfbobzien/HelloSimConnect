efd98d63f1469abb7ed6184c0cb61f2b767ae6ab
William Bobzien <wfbobzien@gmail.com>
2024-02-16 15:33:49 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.FlightSimulator.SimConnect;
+
+namespace HelloSimConnect
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            SimConnect simConnect = new SimConnect("Hello, SimConnect!", IntPtr.Zero, 0, null, 0);
+
+            simConnect.Dispose();
+        }
+    }
+}
